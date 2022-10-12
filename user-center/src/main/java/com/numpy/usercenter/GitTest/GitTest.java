561f4c551c958d41928d6ba61edf88e780ae7110
numpy <731209574@qq.com>
2022-10-13 00:05:17 +0800

    master分支: 这是测试是否能正常更新github项目的版本1

--- user-center/src/main/java/com/numpy/usercenter/GitTest/GitTest.java
+++ user-center/src/main/java/com/numpy/usercenter/GitTest/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         log.info("hello,Git");
         log.info("这是master 分支进行冲突合并test的版本1");
         log.info("这是hot-fix 分支的冲突合并的版本2");
+        System.out.println("这是测试能否更新github项目的版本1");
     }
 }
